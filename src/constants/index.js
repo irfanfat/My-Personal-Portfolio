d15736b37804dda283e7dc966132838eb18e1dba
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2025-05-18 07:51:33 +0000

    refactor: Remove testimonials section from navigation and styles

--- src/constants/index.js
+++ src/constants/index.js
@@ -12,10 +12,6 @@ const navItems = [
     href: "#projects",
   },
   {
-    name: "Testimonials",
-    href: "#testimonials",
-  },
-  {
     name: "Contact",
     href: "#contact",
   },
@@ -125,36 +121,7 @@ const slides = [
   },
 ];
 
-const testimonials = [
-  {
-    name: "John Miller",
-    pos: "Founder of ModernEdge Solutions",
-    review:
-      "David Jhon turned our vision into a stunning, functional platform that our customers love. Their creativity and technical expertise truly set them apart.",
-    imgPath: "/images/client1.png",
-  },
-  {
-    name: "Emily Carter",
-    pos: "UX Designer at PixelWorks Studio",
-    review:
-      "David Jhon consistently brings fresh ideas and innovative solutions. Their passion for creativity and attention to detail elevate every project.",
-    imgPath: "/images/client2.png",
-  },
-  {
-    name: "Sarah Lopez",
-    pos: "Entrepreneur and Small Business Owner",
-    review:
-      "Exceeded my expectations with a unique and beautifully designed product that works flawlessly. Their creative touch is outstanding.",
-    imgPath: "/images/client3.png",
-  },
-  {
-    name: "David Chen",
-    pos: "Project Manager at CreativeSphere Agency",
-    review:
-      "Blends technical skills with bold creativity to deliver exceptional results. They push boundaries and elevate every project they work on.",
-    imgPath: "/images/client4.png",
-  },
-];
+
 
 const footerIconsList = [
   {
@@ -184,6 +151,5 @@ export {
   bentoSocialLinks,
   iconsList,
   slides,
-  testimonials,
   footerIconsList,
 };
